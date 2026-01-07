4c01b905a52092b1604778ce545340933240919f
antoniorb1913 <antoniorb1913@gmail.com>
2026-01-08 00:09:14 +0100

    dockerfile-

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        $this->call([PLayerSeeder::class]);
+        $this->call([PlayerSeeder::class]);
 
         User::factory()->create([
             'name' => 'Test User',
