fd77b4cb611a67165813d4fdb31df4c980a3778d
antoniorb1913 <antoniorb1913@gmail.com>
2026-01-17 00:08:18 +0100

    vercel-version

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,20 @@
 
 use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return redirect()->route('players.index');
 });
 
 Route::resource('players', PlayerController::class);
+
+Route::get('/setup-db', function () {
+    // 1. Ejecutar migraciones (Crea las tablas)
+    Artisan::call('migrate', ['--force' => true]);
+    
+    // 2. Ejecutar Seeders (Llena los datos de jugadores)
+    Artisan::call('db:seed', ['--force' => true]);
+
+    return "Base de datos actualizada y Seeders ejecutados con éxito.";
+});
