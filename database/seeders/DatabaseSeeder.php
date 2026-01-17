ecc0e62577bb3a5baf007ef8d84af054f197988f
antoniorb1913 <antoniorb1913@gmail.com>
2026-01-17 20:05:13 +0100

    prueba en neon

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,14 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // User::factory(10)->create();
+
         $this->call([PlayerSeeder::class]);
 
-        \App\Models\User::updateOrCreate(
-            ['email' => 'test@example.com'], // Si este email existe...
-            [
-                'name' => 'Test User',       // ...actualiza estos datos.
-                'password' => bcrypt('password'), // Si no existe, lo crea.
-            ]
-        );
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+
     }
 }
