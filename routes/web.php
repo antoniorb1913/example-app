4c63c3d61b490c72be6bb128cc32e2e6d0c73c38
antoniorb1913 <antoniorb1913@gmail.com>
2026-01-17 21:20:59 +0100

    prueba en neon

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,26 @@
 use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
-
-Route::get('/', function () {
-    return redirect()->route('players.index');
-});
+//Route::get('/', function () {
+    //return redirect()->route('players.index');
+//})
 
 Route::resource('players', PlayerController::class);
 
+Route::get('/', function () {
+    // Si no existe la tabla de jugadores, configuramos todo automáticamente
+    if (!Schema::hasTable('players')) {
+        try {
+            Artisan::call('migrate', ['--force' => true]);
+            Artisan::call('db:seed', ['--force' => true]);
+            
+            return "Base de datos instalada automáticamente. Refresca la página.";
+        } catch (\Exception $e) {
+            return "Error en auto-instalación: " . $e->getMessage();
+        }
+    }
+    return view('welcome'); // O tu vista principal
+});
