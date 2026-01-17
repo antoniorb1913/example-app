d04b90f394f12d2885cbc345c602bb3354742d4b
antoniorb1913 <antoniorb1913@gmail.com>
2026-01-17 14:12:02 +0100

    vercel-version-final

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,10 @@ use App\Http\Controllers\PlayerController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
-Route::get('/', function () {
-    if (!Schema::hasTable('players')) {
-        Artisan::call('migrate', ['--force' => true]);
-        Artisan::call('db:seed', ['--force' => true]);
-        return "¡Base de datos configurada automáticamente! <a href='/players'>Ver Jugadores</a>";
-    }
-    return view('welcome');
+
+Route::get('/p', function () {
+    return redirect()->route('players.index');
 });
+
+Route::resource('players', PlayerController::class);
